7bf83adf1758a5bcf2b017bec41d6f74070d66bd
Monique Meyer <meyer.monique93@gmail.com>
2022-04-27 14:25:24 +0200

    Added mock

--- project/test/ShoppingFacade.test.ts
+++ project/test/ShoppingFacade.test.ts
@@ -1,28 +1,26 @@
+import { anything, instance, mock, when } from "ts-mockito";
 import ECommerce from "../src/ECommerce";
-import { Order, OrderStatus } from "../src/Order";
+import { OrderStatus } from "../src/Order";
 import PlacedOrders from "../src/Orders";
 import ShoppingFacade from "../src/ShoppingFacade";
 
-class FakePlacedOrders implements PlacedOrders {
-    add(order: Order): boolean {
-        return false;
-    }
-    updateStatus(orderID: number, status: OrderStatus): boolean {
-        return (orderID === 1);
-    }
-}
-
 describe("Shopping Facade E2E", (): void => {
-    let placedOrders = new FakePlacedOrders();
+    // Creating mock
+    let mockedPlacedOrders: PlacedOrders = mock<PlacedOrders>();
+
+    // Getting instance from mock
+    let placedOrders: PlacedOrders = instance(mockedPlacedOrders);
+
     let ecommerce = new ECommerce(placedOrders);
     let shoppingFacade = new ShoppingFacade(ecommerce);
 
     test("cancel non-existing order", (): void => {
+        when(mockedPlacedOrders.updateStatus(anything(), anything())).thenReturn(false);
         expect(shoppingFacade.cancelPlacedOrder(10)).toBeFalsy();
     });
 
     test("cancel existing order", (): void => {
-
+        when(mockedPlacedOrders.updateStatus(1, anything())).thenReturn(true);
         expect(shoppingFacade.cancelPlacedOrder(1)).toBeTruthy();
     });
 
