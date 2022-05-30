4ee16ac4a324d8d730a8c8362cc9408b8f21a69b
Monique Meyer <meyer.monique93@gmail.com>
2022-05-30 11:11:52 +0200

    Added unit tests for PriceCalculator

--- project/src/domain/entity/PriceCalculator.ts
+++ project/src/domain/entity/PriceCalculator.ts
@@ -35,5 +35,3 @@ export default class PriceCalculator {
         })
     }
 }
-
-//TODO move to its own unit tests
--- project/test/PriceCalculator.test.ts
+++ project/test/PriceCalculator.test.ts
@@ -11,17 +11,17 @@ describe("PriceCalculator test", (): void => {
         expect(priceCalculator.calculate()).toBe(0);
     })
 
-    test("Should return 10,50", (): void => {
+    test("Should return 10.50", (): void => {
         priceCalculator.add(10, 1);
         expect(priceCalculator.calculate()).toBe(10.50);
     })
 
-    test("Should return 10,50", (): void => {
+    test("Should return 21.00", (): void => {
         priceCalculator.add(10, 2);
         expect(priceCalculator.calculate()).toBe(21);
     })
 
-    test("Should return 10,50", (): void => {
+    test("Should return 42.00", (): void => {
         priceCalculator.add(10, 2);
         priceCalculator.add(5, 4);
         expect(priceCalculator.calculate()).toBe(42);
