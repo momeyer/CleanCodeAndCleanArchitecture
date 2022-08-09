15498370f73a0e66d6dc105143fa80f7b942a408
Monique Meyer <meyer.monique93@gmail.com>
2022-08-09 13:53:11 +0200

    setting sql mode

--- project/scripts/db.sql
+++ project/scripts/db.sql
@@ -41,14 +41,7 @@ CREATE TABLE
         items TEXT
     );
 
-SET GLOBAL sql_mode = (
-        SELECT
-        REPLACE (
-                @ @sql_mode,
-                'ONLY_FULL_GROUP_BY',
-                ''
-            )
-    );
+SET GLOBAL sql_mode = (SELECT REPLACE(@@sql_mode, 'ONLY_FULL_GROUP_BY', ''));
 
 insert into
     product (
