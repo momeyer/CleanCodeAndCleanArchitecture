1dce5fcc22d75e29a8e0b373edab18e5c5c0d298
Monique Meyer <meyer.monique93@gmail.com>
2022-08-09 13:57:28 +0200

    setting sql mode

--- project/scripts/create_db.sql
+++ project/scripts/create_db.sql
@@ -42,33 +42,3 @@ CREATE TABLE
     );
 
 SET GLOBAL sql_mode = (SELECT REPLACE(@@sql_mode, 'ONLY_FULL_GROUP_BY', ''));
-
-insert into
-    product (
-        id,
-        description,
-        height,
-        width,
-        depth,
-        weight,
-        price
-    )
-values (1, "Camera", 20, 15, 10, 1, 10), (2, "Guitar", 100, 30, 10, 3, 20), (3, "Rubber_Duck", 5, 5, 5, 0.05, 1), (4, "tshirt", 1, 1, 1, 1, 100);
-
-insert into
-    stock_entries (productId, operation, quantity)
-values (1, "in", 100), (2, "in", 100), (3, "in", 100), (4, "in", 100);
-
-INSERT INTO
-    discount_codes(id, amount, code, expire_date)
-VALUES (
-        1,
-        0.3,
-        'get30',
-        '2222-07-22 22:22:22'
-    ), (
-        2,
-        1,
-        "getExpired",
-        '2000-07-25 15:34:50'
-    );
